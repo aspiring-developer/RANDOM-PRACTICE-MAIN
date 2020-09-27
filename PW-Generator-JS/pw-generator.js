71b3faf95b04dbf31141ddd34de8742a23f36d72
Shi Sha <emailtotheowner@gmail.com>
2020-09-27 12:25:19 -0400

    ASCII character processed

--- PW-Generator-JS/pw-generator.js
+++ PW-Generator-JS/pw-generator.js
@@ -18,5 +18,27 @@ function syncRangeAndTextFields(e) {
   characterLengthEl.value = e.target.value;
 }
 
-// TODO Block 3: Sync character range and text field's value
-// A
+// TODO Block 3: Get/Process the ASCII characters
+// Create a function to process the ASCII characters
+function asciiMinMaxValues(min, max) {
+  let asciiCodes = [];
+  for (i = min; i <= max; i++) {
+    asciiCodes.push(i);
+  }
+  return asciiCodes;
+}
+// Get the ASCII character codes and process them
+const uppercaseAscii = asciiMinMaxValues(65, 90);
+const lowercaseAscii = asciiMinMaxValues(97, 122);
+const numbersAscii = asciiMinMaxValues(49, 57);
+const symbolsAscii = asciiMinMaxValues(33, 47)
+  .concat(asciiMinMaxValues(58, 64))
+  .concat(asciiMinMaxValues(91, 96))
+  .concat(asciiMinMaxValues(123, 126));
+// NOTE: The symbols are in different range in ASCII codes table, so needed to combine them with .concat() method
+
+// TODO Block 4: Work on Form submit event
+// Create a "submit-event" event listener to the form
+formFieldEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
