04dbbab9dc0b93892b181bdbcb203b6be2293aea
Shi Sha <emailtotheowner@gmail.com>
2020-09-28 13:46:13 -0400

    generate password function modified

--- PW-Generator-JS/pw-generator.js
+++ PW-Generator-JS/pw-generator.js
@@ -1,32 +1,13 @@
 // TODO Block 1: Get all DOM element reference from the HTML page
-const passwordFieldEl = document.getElementById("passwordDisplayField");
-const formFieldEl = document.getElementById("mainForm");
-const rangeLengthEl = document.getElementById("rangeLength");
-const characterLengthEl = document.getElementById("characterLength");
-const uppercaseChoiceEl = document.getElementById("uppercaseChoiceEl");
-const lowercaseChoiceEl = document.getElementById("lowercaseChoice");
-const numbersChoiceEl = document.getElementById("numbersChoice");
-const symbolsChoiceEl = document.getElementById("symbolsChoice");
+const passwordDisplay = document.getElementById("passwordDisplay");
+const form = document.getElementById("passwordGeneratorForm");
+const characterAmountRange = document.getElementById("characterAmountRange");
+const characterAmountNumber = document.getElementById("characterAmountNumber");
+const includeUppercaseElement = document.getElementById("includeUppercase");
+// const includeLowercaseElement = document.getElementById("lowercaseChoice");
+const includeNumbersElement = document.getElementById("includeNumbers");
+const includeSymbolsElement = document.getElementById("includeSymbols");
 
-// TODO Block 2: Sync character range and text field's value
-// Add "input-event" event listeners to both
-rangeLengthEl.addEventListener("input", syncRangeAndTextFields);
-characterLengthEl.addEventListener("input", syncRangeAndTextFields);
-// Create the function for the syncing "input-event" listeners
-function syncRangeAndTextFields(e) {
-  rangeLengthEl.value = e.target.value;
-  characterLengthEl.value = e.target.value;
-}
-
-// TODO Block 3: Get/Process the ASCII characters
-// Create a function to process the ASCII characters
-function asciiMinMaxValues(min, max) {
-  let asciiCodes = [];
-  for (i = min; i <= max; i++) {
-    asciiCodes.push(i);
-  }
-  return asciiCodes;
-}
 // Get the ASCII character codes and process them
 const uppercaseAscii = asciiMinMaxValues(65, 90);
 const lowercaseAscii = asciiMinMaxValues(97, 122);
@@ -37,21 +18,24 @@ const symbolsAscii = asciiMinMaxValues(33, 47)
   .concat(asciiMinMaxValues(123, 126));
 // NOTE: The symbols are in different range in ASCII codes table, so needed to combine them with .concat() method
 
+// TODO Block 2: Sync character range and text field's value
+// Add "input-event" event listeners to both
+characterAmountRange.addEventListener("input", syncRangeAndTextFields);
+characterAmountNumber.addEventListener("input", syncRangeAndTextFields);
+
 // TODO Block 4: Work on Form submit event
 // Create a "submit-event" event listener to the form
-formFieldEl.addEventListener("submit", function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  const desiredCharacterCount = characterLengthEl.value;
-  const selectedUppercase = uppercaseChoiceEl.checked;
-  //const selectedLowercase = lowercaseChoiceEl.checked;
-  const selectedNumbers = numbersChoiceEl.checked;
-  const selectedSymbols = symbolsChoiceEl.checked;
+  const characterAmount = characterAmountNumber.value;
+  const includeUppercase = includeUppercaseElement.checked;
+  const includeNumbers = includeNumbersElement.checked;
+  const includeSymbols = includeSymbolsElement.checked;
   const password = generatePassword(
-    desiredCharacterCount,
-    selectedUppercase,
-    //selectedLowercase,
-    selectedNumbers,
-    selectedSymbols
+    characterAmount,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
   );
   passwordFieldEl.innerText = password;
 });
@@ -59,25 +43,17 @@ formFieldEl.addEventListener("submit", function (e) {
 // TODO Block 5: Generate the password
 // Create a function to generate password
 function generatePassword(
-  desiredCharacterCount,
-  selectedUppercase,
-  //selectedLowercase,
-  selectedNumbers,
-  selectedSymbols
+  characterAmount,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
 ) {
-  let getCharacters = selectedLowercase;
-  if (selectedUppercase) {
-    getCharacters = getCharacters.concat(uppercaseAscii);
-  }
-  // if (selectedLowercase) {
-  //   getCharacters = getCharacters.concat(lowercaseAscii);
-  // }
-  if (selectedNumbers) {
-    getCharacters = getCharacters.concat(numbersAscii);
-  }
-  if (selectedSymbols) {
-    getCharacters = getCharacters.concat(symbolsAscii);
-  }
+  let charCodes = lowercaseAscii;
+  if (includeUppercase) charCodes = charCodes.concat(uppercaseAscii);
+
+  if (includeNumbers) charCodes = charCodes.concat(numbersAscii);
+
+  if (includeSymbols) charCodes = charCodes.concat(symbolsAscii);
 
   const collectedCharacters = [];
   for (i = 0; i < desiredCharacterCount; i++) {
@@ -87,4 +63,19 @@ function generatePassword(
   }
   //return collectedCharacters;
   return collectedCharacters.join("");
+}
+// TODO Block 3: Get/Process the ASCII characters
+// Create a function to process the ASCII characters
+function asciiMinMaxValues(min, max) {
+  let asciiCodes = [];
+  for (i = min; i <= max; i++) {
+    asciiCodes.push(i);
+  }
+  return asciiCodes;
+}
+
+// Create the function for the syncing "input-event" listeners
+function syncRangeAndTextFields(e) {
+  characterAmountNumber.value = e.target.value;
+  characterAmountRange.value = e.target.value;
 }
