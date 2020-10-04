c2255f136fc1da83dcfed96bd546c1890102ad5e
Shi Sha <emailtotheowner@gmail.com>
2020-10-04 14:35:09 -0400

    Retrieved DOM elements from HTML page

--- NEW-PW-GEN-PRA-1/mainScript.js
+++ NEW-PW-GEN-PRA-1/mainScript.js
@@ -1,2 +1,19 @@
 // Retrieve the DOM elements
-const form = document.getElementById("");
+const form = document.getElementById("mainForm");
+const rangeSliderEl = document.getElementById("Input_CharacterRange");
+const numberCounterEl = document.getElementById("Input_CharacterNumber");
+const lowercaseEl = document.getElementById("Input_Lowercase");
+const uppercaseEl = document.getElementById("Input_Uppercase");
+const numbersEl = document.getElementById("Input_Numbers");
+const symbolsEl = document.getElementById("Input_Symbols");
+const submitEl = document.getElementById("submitButton");
+
+// Add event listener to the button
+
+// Sync range and counter
+
+// Create
+
+// Create ASCII characters for password choices
+
+// Create function to generate password
