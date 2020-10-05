9473e456a8e4b73e784f0c218d77da5275a9cf55
Shi Sha <emailtotheowner@gmail.com>
2020-10-05 13:33:17 -0400

    Ascii loop created

--- PW-GEN-PRA/practice-2-10052020/mainScript-2.js
+++ PW-GEN-PRA/practice-2-10052020/mainScript-2.js
@@ -2,9 +2,27 @@
 const passwordDisplayEL = document.getElementById("passwordDisplay");
 const formEl = document.getElementById("passwordGeneratorForm");
 const characterAmountRangeEl = document.getElementById("characterAmountRange");
-const characterAmountNumberEl = document.getElementById(
-  "characterAmountNumber"
-);
+const characterAmountNumberEl = document.getElementById("characterAmountNumber");
 const includeUppercaseEl = document.getElementById("includeUppercase");
 const includeNumbersEl = document.getElementById("includeNumbers");
 const includeSymbolsEl = document.getElementById("includeSymbols");
+
+// Sync range and counter field
+characterAmountRangeEl.addEventListener("input", syncRangeAndCounter);
+characterAmountNumberEl.addEventListener("input", syncRangeAndCounter);
+function syncRangeAndCounter(e) {
+  const syncRangeAndCounterValue = e.target.value;
+  characterAmountRangeEl.value = syncRangeAndCounterValue;
+  characterAmountNumberEl.value = syncRangeAndCounterValue;
+}
+
+// Create a function to loop the ASCII character lists
+function asciiBeginAndEnd(begin, end) {
+  let asciiLoop = [];
+  for(i=begin; i<=end; i++){
+  asciiLoop.push(i);
+  }
+  //console.log(asciiLoop)
+  return asciiLoop;
+}
+asciiBeginAndEnd(65, 90);
