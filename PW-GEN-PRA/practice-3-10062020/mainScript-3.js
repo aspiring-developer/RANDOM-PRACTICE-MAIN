4e2de910dcbc07d4856e73a538334ae5d17fbe68
Shi Sha <emailtotheowner@gmail.com>
2020-10-06 07:25:38 -0400

    submit event listener complete

--- PW-GEN-PRA/practice-3-10062020/mainScript-3.js
+++ PW-GEN-PRA/practice-3-10062020/mainScript-3.js
@@ -1,5 +1,6 @@
 // Get all DOM elements
 const formEl = document.getElementById("passwordGeneratorForm");
+const passwordFieldEl = document.getElementById("passwordDisplay");
 const rangeEl = document.getElementById("characterAmountRange");
 const counterEl = document.getElementById("characterAmountNumber");
 const uppercaseEl = document.getElementById("includeUppercase");
@@ -30,5 +31,21 @@ asciiStartAndEnd (65, 90)
 const upperAscii = asciiStartAndEnd(65, 90);
 const lowerAscii = asciiStartAndEnd(97, 122);
 const numbersAscii = asciiStartAndEnd(48, 57);
-const symbolsAscii = asciiStartAndEnd(33, 47).concat(asciiStartAndEnd(65, 90));
-// UC: 65-90, LC: 97-122, Num: 48-57, (Sym: 33-47, 58-64, 91-96, 123-126)
+const symbolsAscii = asciiStartAndEnd(33, 47).concat(asciiStartAndEnd(58, 64)).concat(asciiStartAndEnd(91, 96)).concat(asciiStartAndEnd(123, 126));
+
+// Create "submit-event" event listener to the form
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const counterNumberDesired = counterEl.value;
+  const uppercaseSelect = uppercaseEl.value;
+  const lowercaseSelect = lowercaseEl.value;
+  const numbersSelect = numbersEl.value;
+  const symbolsSelect = symbolsEl.value;
+  const password = generatePassword(uppercaseSelect, lowercaseSelect, numbersSelect, symbolsSelect);
+  passwordFieldEl.innerText = password;
+});
+
+// Create the generatePassword function
+function generatePassword(uppercaseSelect, lowercaseSelect, numbersSelect, symbolsSelect) {
+
+}
