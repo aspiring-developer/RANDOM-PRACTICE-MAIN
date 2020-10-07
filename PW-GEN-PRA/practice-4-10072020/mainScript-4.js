b340c7dc169901238d0c2fb204bdf46c786e278f
Shi Sha <emailtotheowner@gmail.com>
2020-10-07 10:52:15 -0400

    Ascii characters retrieved

--- PW-GEN-PRA/practice-4-10072020/mainScript-4.js
+++ PW-GEN-PRA/practice-4-10072020/mainScript-4.js
@@ -23,8 +23,19 @@ function asciiStartAndEnd(start, end) {
   //console.log(asciiRetrieve);
 }
 //asciiStartAndEnd(1, 8)
+
 // Retrieve ASCII characters to process
+const lowerAscii = asciiStartAndEnd(97, 122);
+const upperAscii = asciiStartAndEnd(65, 90);
+const upperAscii = asciiStartAndEnd(48, 57);
+const upperAscii = asciiStartAndEnd(33, 47).concat(asciiStartAndEnd(58, 64)).concat(asciiStartAndEnd(91, 96)).concat(asciiStartAndEnd(123, 126));
 
 // Create submit-event to the form
 
 // Create generate function
+
+
+
+
+// TODO: Reference ASCII Values
+// UC: 65, 90  |  LC: 97, 122 |  Num: 48, 57 | (Sym: 33, 47  &&  58, 64  &&  91, 96  &&  123, 126  )
