89c26df8df729d40b5a20d191811bada0b9ab808
Shi Sha <emailtotheowner@gmail.com>
2020-10-06 20:11:55 -0400

    Third math operation complete

--- MATH-OPE-PRA/Math-Op-3-Form-Select-Option/mainScript-3.js
+++ MATH-OPE-PRA/Math-Op-3-Form-Select-Option/mainScript-3.js
@@ -1,34 +1,31 @@
 // Retrieve DOM elements
 const formEl = document.getElementById("operatorForm");
 const resultDisplayEl = document.getElementById("resultDisplay");
+const operatorOptionEl = document.getElementById("operatorOption");
 const inputField1El = document.getElementById("inputField1");
 const inputField2El = document.getElementById("inputField2");
-const addEl = document.querySelector(".add");
-const subtractEl = document.querySelector(".subtract");
-const multiplyEl = document.querySelector(".multiply");
-const divideEl = document.querySelector(".divide");
+const calculateBtnEl = document.getElementById("calculateBtn");
 
 // Add submit-event event listener to the form
-formEl.addEventListener("submit", function (e) {
+calculateBtnEl.addEventListener("click", calculate);
+function calculate(e) {
   e.preventDefault();
-  addEl.addEventListener("click", doAddition);
-  function doAddition() {
-    let addition = parseInt(inputField1El.value) + parseInt(inputField2El.value);
-    resultDisplayEl.innerText = addition;
-   }
-  subtractEl.addEventListener("click", doSubtraction);
-  function doSubtraction() {
-    let subtraction = parseInt(inputField1El.value) - parseInt(inputField2El.value);
-    resultDisplayEl.innerText = subtraction;
-  }
-  multiplyEl.addEventListener("click", doMultiplication);
-  function doMultiplication() {
-    let multiplication = parseInt(inputField1El.value) * parseInt(inputField2El.value);
-    resultDisplayEl.innerText = multiplication;
-  }
-  divideEl.addEventListener("click", doDivision);
-  function doDivision() {
-    let division = parseInt(inputField1El.value) / parseInt(inputField2El.value);
-    resultDisplayEl.innerText = division;
-  }
-});
+  let optionChosen = operatorOptionEl.value;
+  console.log(optionChosen);
+     if(optionChosen === "Add") {
+        resultDisplayEl.innerText = parseInt(inputField1El.value) + parseInt(inputField2El.value);
+    } else if(optionChosen === "Subtract") {
+      resultDisplayEl.innerText = parseInt(inputField1El.value) - parseInt(inputField2El.value);
+    } else if(optionChosen === "Multiply") {
+      resultDisplayEl.innerText = parseInt(inputField1El.value) * parseInt(inputField2El.value);
+    } else if(optionChosen === "Divide") {
+      resultDisplayEl.innerText = parseInt(inputField1El.value) / parseInt(inputField2El.value);
+    } else {
+     resultDisplayEl.innerHTML = ` <h4 style="color:red"> Select an operator option! </h4> `;
+    }
+    if(inputField1El.value === "" && inputField2El.value === '') {
+      resultDisplayEl.innerHTML = ` <h4 style="color:red"> Type both numbers! </h4> `;
+    }
+      }
+
+
