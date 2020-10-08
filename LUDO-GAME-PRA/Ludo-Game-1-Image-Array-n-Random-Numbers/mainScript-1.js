be92f06007be86f3c26441db196a6989342dd7a3
Shi Sha <emailtotheowner@gmail.com>
2020-10-08 18:48:53 -0400

    DOM element retrieved and images array created

---
+++ LUDO-GAME-PRA/Ludo-Game-1-Image-Array-n-Random-Numbers/mainScript-1.js
@@ -0,0 +1,9 @@
+// Retrieve DOM elements
+const player1El = document.getElementById("player1");
+const player2El = document.getElementById("player2");
+const rollBtnEl = document.getElementById("rollBtn1");
+
+// Create dice-roll images array
+const diceImageArray = ["./ludo-images/dice-1.png", "./ludo-images/dice-2.png", "./ludo-images/dice-3.png", "./ludo-images/dice-4.png", "./ludo-images/dice-5.png", "./ludo-images/dice-6.png",]
+
+// Add event listener to the button
