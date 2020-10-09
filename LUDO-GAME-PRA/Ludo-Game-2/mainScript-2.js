e6237d9e43822054203ced6a78bab7713b86dad1
Shi Sha <emailtotheowner@gmail.com>
2020-10-09 06:58:40 -0400

    DOM element retrieved

---
+++ LUDO-GAME-PRA/Ludo-Game-2/mainScript-2.js
@@ -0,0 +1,9 @@
+// Retrieve the DOM elements
+const resultDisplayEl = document.getElementById("resultDisplay");
+const player1El = document.getElementById("player1");
+const player2El = document.getElementById("player2");
+const rollBtnEl = document.getElementById("rollBtn");
+
+// Create images array
+const diceImagesArray = [""]
+
