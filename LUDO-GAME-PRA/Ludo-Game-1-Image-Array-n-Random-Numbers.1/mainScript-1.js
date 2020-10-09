b551a741922cad39f97decbabb6282ddd149f19f
Shi Sha <emailtotheowner@gmail.com>
2020-10-09 06:51:25 -0400

    New project created

--- LUDO-GAME-PRA/Ludo-Game-1-Image-Array-n-Random-Numbers.1/mainScript-1.js
+++ LUDO-GAME-PRA/Ludo-Game-1-Image-Array-n-Random-Numbers.1/mainScript-1.js
@@ -5,14 +5,7 @@ const rollBtnEl = document.getElementById("rollBtn");
 const resultDisplayEl = document.getElementById("resultDisplay");
 
 // Create dice-roll images array
-const diceImageArray = [
-  "./ludo-images/dice-1.png",
-  "./ludo-images/dice-2.png",
-  "./ludo-images/dice-3.png",
-  "./ludo-images/dice-4.png",
-  "./ludo-images/dice-5.png",
-  "./ludo-images/dice-6.png",
-];
+const diceImageArray = ["./ludo-images/dice-1.png", "./ludo-images/dice-2.png", "./ludo-images/dice-3.png", "./ludo-images/dice-4.png", "./ludo-images/dice-5.png", "./ludo-images/dice-6.png"];
 
 // Add event listener to the button
 rollBtnEl.addEventListener("click", rollDice);
@@ -20,14 +13,10 @@ rollBtnEl.addEventListener("click", rollDice);
 // Create rollDice function
 function rollDice() {
   for (i = 1; i <= diceImageArray.length; i++) {
-    let player1Rolled =
-      diceImageArray[Math.floor(Math.random() * diceImageArray.length)];
-    //console.log(player1Rolled);
+    let player1Rolled = diceImageArray[Math.floor(Math.random() * diceImageArray.length)];
     player1El.innerHTML = ` <img src='${player1Rolled}' /> `;
 
-    let player2Rolled =
-      diceImageArray[Math.floor(Math.random() * diceImageArray.length)];
-    //console.log(player2Rolled);
+    let player2Rolled = diceImageArray[Math.floor(Math.random() * diceImageArray.length)];
     player2El.innerHTML = ` <img src='${player2Rolled}' /> `;
 
     if (player1El.innerHTML > player2El.innerHTML) {
