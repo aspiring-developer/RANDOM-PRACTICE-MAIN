dc9db108c84d9e0bba8f23d55906159e806a95dc
Shi Sha <emailtotheowner@gmail.com>
2020-10-09 07:00:27 -0400

    Images array created

--- LUDO-GAME-PRA/Ludo-Game-2/mainScript-2.js
+++ LUDO-GAME-PRA/Ludo-Game-2/mainScript-2.js
@@ -5,5 +5,5 @@ const player2El = document.getElementById("player2");
 const rollBtnEl = document.getElementById("rollBtn");
 
 // Create images array
-const diceImagesArray = [""]
+const diceImagesArray = ["./ludo-images-2/dice-1.png", "./ludo-images-2/dice-1.png", "./ludo-images-2/dice-1.png", "./ludo-images-2/dice-1.png", "./ludo-images-2/dice-1.png", "./ludo-images-2/dice-1.png"]
 
