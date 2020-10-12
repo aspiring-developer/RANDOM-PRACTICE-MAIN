d39a3c80b6ce3463095459c290a7072753e591a5
Shi Sha <emailtotheowner@gmail.com>
2020-10-12 18:30:42 -0400

    app complete

--- PW-GEN-PRA/practice-6-10122020/mainScript-6.js
+++ PW-GEN-PRA/practice-6-10122020/mainScript-6.js
@@ -17,11 +17,50 @@ function syncRangeAndCounter(e) {
 
 // Create a function with a loop for ASCII characters process
 function asciiStartAndEnd(start, end) {
-
+  let asciiLoop = [];
+  for (i = start; i <= end; i++) {
+    asciiLoop.push(i);
+  }
+  return asciiLoop;
 }
+//asciiStartAndEnd(97, 122);
+
+const lowerAscii = asciiStartAndEnd(97, 122);
+const upperAscii = asciiStartAndEnd(65, 90);
+const numbersAscii = asciiStartAndEnd(48, 57);
+let symbolsAscii = asciiStartAndEnd(33, 47).concat(asciiStartAndEnd(58, 64)).concat(asciiStartAndEnd(91, 96)).concat(asciiStartAndEnd(123, 126));
 
 // Create submit event listener to the form
-
+passwordGeneratorFormEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log("WORKING!")
+  const characterCount = characterAmountNumberEl.value;
+  const includeUppercase = includeUppercaseEl.checked;
+  const includeNumbers = includeNumbersEl.checked;
+  const includeSymbols = includeSymbolsEl.checked;
+  const password = generatePassword(characterCount, includeUppercase, includeNumbers, includeSymbols);
+  passwordDisplayEl.innerHTML = password;
+})
 
 // Create generatePassword function
+function generatePassword(characterCount, includeUppercase, includeNumbers, includeSymbols) {
+  console.log(characterCount, includeUppercase, includeNumbers, includeSymbols);
+  let passwordAscii = lowerAscii;
 
+  if(includeUppercase) {passwordAscii = passwordAscii.concat(upperAscii)};
+  if(includeNumbers) {passwordAscii = passwordAscii.concat(numbersAscii)};
+  if(includeSymbols) {passwordAscii = passwordAscii.concat(symbolsAscii)};
+
+  let passwordCharacters = [];
+
+  for (i = 0; i < characterCount; i++) {
+    let finalPassword = passwordAscii[Math.floor(Math.random() * passwordAscii.length)];
+    passwordCharacters.push(String.fromCharCode(finalPassword));
+    }
+  return passwordCharacters.join("");
+  }
+
+
+
+// TODO: Reference ASCII Values
+// UC: 65, 90  |  LC: 97, 122 |  Num: 48, 57 | (Sym: 33, 47  &&  58, 64  &&  91, 96  &&  123, 126  )
