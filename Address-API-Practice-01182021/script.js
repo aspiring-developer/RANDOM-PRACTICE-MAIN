4d38315eb1a581b376c191e5aefce8dc0cd64cbc
ssh1sharma <emailtotheowner@gmail.com>
2021-01-18 09:12:25 -0500

    Address API added

---
+++ Address-API-Practice-01182021/script.js
@@ -0,0 +1,10 @@
+//alert("It is working!");
+
+//L.mapquest.key = 'KEY';
+
+//// 'map' refers to a <div> element with the ID map
+//L.mapquest.map('map', {
+//  center: [37.7749, -122.4194],
+//  layers: L.mapquest.tileLayer('map'),
+//  zoom: 12
+//});
