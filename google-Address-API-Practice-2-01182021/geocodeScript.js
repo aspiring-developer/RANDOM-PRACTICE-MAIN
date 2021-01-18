418c00c2ea1cc82e8a97585cc93b167f9d75eebc
ssh1sharma <emailtotheowner@gmail.com>
2021-01-18 16:32:01 -0500

    Geocode mapping completed

---
+++ google-Address-API-Practice-2-01182021/geocodeScript.js
@@ -0,0 +1,34 @@
+geocode();
+function geocode(e) {
+e.preventDefault();
+let location = document.getElementById("locationInput").value;
+
+axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+  params: {
+      address: location,
+      key: "AIzaSyBNlqKXOpDlK_yYaaFqLpacVf4mtrc7wRI"
+  }
+ }).then(function(response) {
+console.log(response);
+let formattedAddress = response.data.results[0].formatted_address;
+let formattedAddressOutput = `
+<ul class="list-group">
+<li class="list-group-item"> ${formattedAddress} </li>
+</ul>
+`;
+
+// Address
+let addressComponents = response.data.results[0].address_components;
+console.log(addressComponents);
+let addressComponentsOutput = '<ul class="list-group">';
+for (let i = 0; i < addressComponents.length; i++) {
+addressComponentsOutput += `
+<li class="list-group-item"> ${addressComponents[i].types[0]} : ${addressComponents[i].long_name} </li> `;
+}
+addressComponentsOutput +=  '</ul>';
+document.getElementById("mainAddressDisplay").innerHTML = formattedAddressOutput;
+document.getElementById("addressComponentsDisplay").innerHTML = addressComponentsOutput;
+ }).catch(function(error) {
+   console.log(error);
+ });
+}
--- google-Address-API-Practice-2-01182021/mapScript.js
+++ google-Address-API-Practice-2-01182021/mapScript.js
@@ -1,11 +1,9 @@
-//Map script
-
- //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBNlqKXOpDlK_yYaaFqLpacVf4mtrc7wRI
     let map;
 
     function initMap() {
       // This Lat/Lng is for Apex, NC
       let myLatLng = { lat: 35.835292, lng: -78.606045 };
+
       map = new google.maps.Map(document.getElementById("map"), {
         center: myLatLng,
         zoom: 8,
