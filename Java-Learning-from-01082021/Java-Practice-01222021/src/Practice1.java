23c9dcf1952e3f660b686ed0687e32e4009e87ca
ssh1sharma <emailtotheowner@gmail.com>
2021-01-22 14:27:24 -0500

    Some file structure update

--- Java-Learning-from-01082021/Java-Practice-01222021/src/Practice1.java
+++ Java-Learning-from-01082021/Java-Practice-01222021/src/Practice1.java
@@ -1,17 +1,34 @@
-public class Practice1 {
-int i = 12;
-int j = 24;
+// REGULAR WORKING CODES
+//public class Practice1 {
+//int i = 12;
+//int j = 24;
+//
+//public void result() {
+//  int k = i + j;
+//  System.out.println(k);
+//}
+//
+//  public static void main(String[] args) {
+//Practice1 p = new Practice1();
+//    p.result();
+//    System.out.println(p.i);
+//    System.out.println(p.j);
+//  }
+//}
 
-public void result() {
-  int k = i + j;
-  //return k;
-  System.out.println(k);
-}
+// TRYING THROUGH CONSTRUCTOR
+public class Practice1 {
+  int i = 12;
+  int j = 24;
+
+  public Practice1() {
+    int k = i + j;
+    System.out.println(k);
+  }
 
   public static void main(String[] args) {
-Practice1 p = new Practice1();
-    p.result();
-    System.out.println(p.i);
-    System.out.println(p.j);
+    Practice1 pObject = new Practice1();
+    System.out.println(pObject.i);
+    System.out.println(pObject.j);
   }
 }
