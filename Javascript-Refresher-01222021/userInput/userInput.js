c3835e0530cb031f5773a91433eec1b42d76e99d
ssh1sharma <emailtotheowner@gmail.com>
2021-01-23 13:27:26 -0500

    Completion of user input

--- Javascript-Refresher-01222021/userInput/userInput.js
+++ Javascript-Refresher-01222021/userInput/userInput.js
@@ -1,16 +1,16 @@
 let inputFormEl = document.getElementById("inputForm");
 let inputFieldEl = document.getElementById("inputField");
-let displayButtonEl = document.getElementById("displayButton");
 let itemListsEl  = document.getElementById("itemLists");
 
 inputFormEl.addEventListener("submit", displayTextsFunction);
 
-
 function displayTextsFunction(e) {
   e.preventDefault();
-  itemListsEl.insertAdjacentHTML('beforebegin', `<li class="my-2 p-2"> ${inputFieldEl.value} <button class="btn btn-danger ml-2 p-1"> Delete </button> </li>`);
+  itemListsEl.insertAdjacentHTML('afterbegin', `<li class="my-2 p-2"> ${inputFieldEl.value} <button class="btn btn-danger ml-2 p-1" onclick="deleteListFunction(this)" id="deleteButton"> Delete </button> </li>`);
   inputFieldEl.value = "";
   inputFieldEl.focus();
 }
 
-
+function deleteListFunction(itemToDelete) {
+  itemToDelete.parentElement.remove();
+}
