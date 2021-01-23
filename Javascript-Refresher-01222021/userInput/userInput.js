e86b1a89f266aeaa9ed5ae2f0bf8548dfa542fa4
ssh1sharma <emailtotheowner@gmail.com>
2021-01-23 10:31:10 -0500

    User Input working

--- Javascript-Refresher-01222021/userInput/userInput.js
+++ Javascript-Refresher-01222021/userInput/userInput.js
@@ -1,11 +1,16 @@
-let textDisplayEl = document.getElementById("textDisplay");
+let inputFormEl = document.getElementById("inputForm");
+let inputFieldEl = document.getElementById("inputField");
 let displayButtonEl = document.getElementById("displayButton");
+let itemListsEl  = document.getElementById("itemLists");
 
-displayButtonEl.addEventListener("click", displayTextsFunction);
+inputFormEl.addEventListener("submit", displayTextsFunction);
 
 
-function displayTextsFunction() {
-
+function displayTextsFunction(e) {
+  e.preventDefault();
+  itemListsEl.insertAdjacentHTML('beforebegin', `<li class="my-2 p-2"> ${inputFieldEl.value} <button class="btn btn-danger ml-2 p-1"> Delete </button> </li>`);
+  inputFieldEl.value = "";
+  inputFieldEl.focus();
 }
 
 
