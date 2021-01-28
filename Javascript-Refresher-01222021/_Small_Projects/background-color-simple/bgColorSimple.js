4443a07612d4827cb882c37ce7c3ee32d42bdeea
ssh1sharma <emailtotheowner@gmail.com>
2021-01-28 15:30:40 -0500

    bg color changing practice complete

--- Javascript-Refresher-01222021/_Small_Projects/background-color-simple/bgColorSimple.js
+++ Javascript-Refresher-01222021/_Small_Projects/background-color-simple/bgColorSimple.js
@@ -1,4 +1,17 @@
 let colorButtonEl = document.getElementById("colorButton");
 let backgroundEl = document.querySelector("body");
 
+colorButtonEl.addEventListener("click", changeBg);
 
+let bgColor = ["red", "green", "blue", "purple", "#aeaeae", "orange", "brown", "yellow"]
+
+function changeBg() {
+let randomizedBgColor = Math.floor(Math.random() * bgColor.length);
+backgroundEl.style.backgroundColor = bgColor[randomizedBgColor];
+  }
+changeBg();
+
+resetting();
+function resetting() {
+  backgroundEl.style.backgroundColor = "#fff";
+}
