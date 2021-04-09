2be708dc3c132835a01f74833a6d97e89a480371
ssh1sharma <emailtotheowner@gmail.com>
2021-04-09 10:24:58 -0400

    DOM manipulating with vanilla JS initial commit

--- JS-Refresher-04052021/script-for-dom_test.js
+++ JS-Refresher-04052021/script-for-dom_test.js
@@ -3,3 +3,24 @@ const inputFieldEl = document.querySelector('.input-field');
 const taskEl = document.querySelector('#task');
 const filterEl = document.querySelector('#filter');
 const collectionEl = document.querySelector('.collection');
+
+taskFormEl.addEventListener('submit', addTask);
+
+function addTask(e) {
+  e.preventDefault();
+  // create li
+  let li = document.createElement('li');
+// get the user input value in li
+  let input = document.createTextNode(taskEl.value);
+  li.appendChild(input);
+  // add anchor element
+  let link = document.createElement('a');
+  link.innerHTML = '<i class="fa fa-remove secondary-content"></i>';
+  li.appendChild(link);
+
+  console.log(li, taskEl.value, link)
+
+  // add li to ul
+  collectionEl.appendChild(li);
+
+}
