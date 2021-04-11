d0bfce86588b23add65a180f7129f4616252c22f
ssh1sharma <emailtotheowner@gmail.com>
2021-04-11 15:40:39 -0400

    Project phase 1 complte

--- JS-Refresher-04052021/script-for-dom-test2.js
+++ JS-Refresher-04052021/script-for-dom-test2.js
@@ -1,23 +1,19 @@
-const taskFormEl = document.querySelector('#task-form');
-const taskInputEl = document.querySelector('#task');
-const filterEl = document.querySelector('#filter');
+const formEl = document.getElementById('task-form');
+const taskInputEl = document.getElementById('task');
 const taskListEl = document.querySelector('.collection');
-const clearBtnEl = document.querySelector('.clear-tasks');
-
-taskFormEl.addEventListener('submit', addTask);
+const clearTasksEl = document.querySelector('.clear-tasks');
+const filterEl = document.getElementById('filter');
 
 // ADD FEATURE
+formEl.addEventListener('submit', addTask);
 function addTask(e) {
   e.preventDefault();
   const li = document.createElement('li');
   li.className = 'collection-item';
-  const liText = document.createTextNode(taskInputEl.value);
-  li.appendChild(liText);
-
+  li.append(document.createTextNode(taskInputEl.value));
   const link = document.createElement('a');
-  link.innerHTML = '<i class="fa fa-remove secondary-content delete-icon"></i>';
+  link.innerHTML = '<i class="fa fa-remove secondary-content delete-me"></i>';
   li.appendChild(link);
-
   taskListEl.appendChild(li);
 
   taskInputEl.value = '';
@@ -27,29 +23,28 @@ function addTask(e) {
 // DELETE FEATURE
 taskListEl.addEventListener('click', deleteTask);
 function deleteTask(e) {
-  if (e.target.classList.contains('delete-icon')) {
+  if (e.target.classList.contains('delete-me')) {
     e.target.parentElement.parentElement.remove();
   }
 }
 
 // CLEAR FEATURE
-clearBtnEl.addEventListener('click', clearTask);
-function clearTask() {
+clearTasksEl.addEventListener('click', clearTasks);
+function clearTasks() {
   taskListEl.remove();
 }
 
 // FILTER FEATURE
 filterEl.addEventListener('keyup', filterTask);
-function filterTask(e) {
-  let userInputText = e.target.value;
-  console.log(userInputText);
-  let taskItemInList = document.querySelectorAll('.collection-item');
-  taskItemInList.forEach(function (eachItem) {
-    if (eachItem.firstChild.textContent.indexOf(userInputText) !== -1) {
-      eachItem.style.display = 'block';
-    } else {
-      eachItem.style.display = 'none';
-    }
-  })
+function filterTask() {
+let searchingText = filterEl.value;
+let taskInList = document.querySelectorAll('.collection-item');
+taskInList.forEach(function(eachItem) {
+  let eachItemText = eachItem.textContent;
+  if(eachItemText.indexOf(searchingText) !== -1) {
+    eachItem.style.display = 'block';
+  } else {
+    eachItem.style.display = 'none';
+  }
+})
 }
-
