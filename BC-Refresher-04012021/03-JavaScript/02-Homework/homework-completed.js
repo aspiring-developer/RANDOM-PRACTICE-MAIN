6c982c1310c3b9d533299aa331a1f88f361b4f2a
ssh1sharma <emailtotheowner@gmail.com>
2021-04-15 13:14:56 -0400

    BC hw of js

--- BC-Refresher-04012021/03-JavaScript/02-Homework/homework-completed.js
+++ BC-Refresher-04012021/03-JavaScript/02-Homework/homework-completed.js
@@ -1,24 +1,52 @@
 const characterCounterEl = document.querySelector('#characterCounter');
 const lowerCheckboxEl = document.querySelector('#lowerCheckbox');
+const upperCheckboxEl = document.querySelector('#upperCheckbox');
 const numberCheckboxEl = document.querySelector('#numberCheckbox');
 const specialCheckboxEl = document.querySelector('#specialCheckbox');
 const passwordResultEl = document.querySelector('#passwordResult');
 const passwordButtonEl = document.querySelector('#passwordButton');
+const eachCheckBoxEl = document.querySelectorAll('.eachCheckBox');
 
 passwordButtonEl.addEventListener('click', generatePassword);
-function generatePassword() {
-const lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
+const lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const copyCharacter = lowerCaseCharacter.slice(0);
-let upperCaseCharacter = copyCharacter.map(function(eachChar) {
-return eachChar.toUpperCase();
+let upperCaseCharacter = copyCharacter.map(function (eachChar) {
+  return eachChar.toUpperCase();
 });
 
-const specialCharacter = ["'!','#','$','%','&','?','@','_'"];
-const numberCharacter = [0,1,2,3,4,5,6,7,8,9];
-console.log(lowerCaseCharacter)
-//console.log(copyCharacter)
-console.log(upperCaseCharacter)
-console.log(specialCharacter)
-console.log(numberCharacter)
-  console.log("The function is working...!")
+const specialCharacter = ['!', '#', '$', '%', '&', '?', '@', '_'];
+const numberCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+function generatePassword() {
+  selectedCharacter();
 }
+
+
+let checkedCharacters = [];
+function selectedCharacter() {
+  // Checking if the character option is checked or not
+  if (!lowerCheckboxEl.checked && !upperCheckboxEl.checked && !numberCheckboxEl.checked && !specialCheckboxEl.checked) {
+    alert("You must check at least one character option!");
+  }
+
+  // Collecting selected character option
+  if (lowerCheckboxEl.checked) { checkedCharacters.push(lowerCaseCharacter); }
+  if (upperCheckboxEl.checked) { checkedCharacters.push(upperCaseCharacter); }
+  if (numberCheckboxEl.checked) { checkedCharacters.push(numberCharacter); }
+  if (specialCheckboxEl.checked) { checkedCharacters.push(specialCharacter); }
+  //console.log(checkedCharacters);
+  //return checkedCharacters;
+
+  // Randomising selected characters
+  function randomizeCharacter(random) {
+    let randomizedArray = [];
+    for (let i = 0; i < checkedCharacters.length; i++) {
+      randomizedArray.push(random[i].sort(() => Math.random() - 0.5));
+    }
+    console.log(randomizedArray);
+  }
+  randomizeCharacter(checkedCharacters)
+}
+
