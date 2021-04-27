6e6a713b92c6e2ea978bdcd9ad6053d4bb7c62b9
ssh1sharma <emailtotheowner@gmail.com>
2021-04-27 07:51:09 -0400

    Object and Array iteration comparison

--- JS-JavaScript-04192021/testing.js
+++ JS-JavaScript-04192021/testing.js
@@ -1,14 +1,55 @@
+// Object
 let car = {
   name: "Toyota",
   model: "Corolla",
   year: 1997,
   hasWifi: false
 }
-console.log(car.name);
-for (let i = 0; i < car.length; i++) {
-  const element = car[i];
-  console.log(element);
-  console.log("For Loop");
-
+for (let i in car) {
+  console.log(car[i]);
 }
 
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+// Array
+let car1 = [
+  "Toyota",
+  "Corolla",
+  1997,
+  false
+]
+for (let j of car1) {
+  console.log(j);
+}
+
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+const object = { a: 1, b: 2, c: 3 };
+
+for (const property in object) {
+  console.log(`${property}: ${object[property]}`);
+}
+
+// expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+const array1 = ['a', 'b', 'c'];
+
+for (const element of array1) {
+  console.log(element);
+}
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+
+/////////////////////////////////////////////
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
