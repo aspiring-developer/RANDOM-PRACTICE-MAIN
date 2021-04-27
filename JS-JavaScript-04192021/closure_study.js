70c791f3cfaae057715612f930e188f3a082db1f
ssh1sharma <emailtotheowner@gmail.com>
2021-04-27 07:34:38 -0400

    New testing environment initialized

--- JS-JavaScript-04192021/closure_study.js
+++ JS-JavaScript-04192021/closure_study.js
@@ -1,27 +1,45 @@
 /////////////////////////////////////////////////////
-// Study # 1  //
+console.log("Study # 1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
 function instructionGenerator() {
   function multiplyBy2(num) {
     return num * 2;
   }
-  return multiplyBy2;
+  return multiplyBy2; // NOT invoking, BUT returning function definition!
 }
 const generatedFunc = instructionGenerator();
 console.log(generatedFunc); // RESULT -->  Returns whole function definition like below:
 /* ƒ multiplyBy2(num) {
   return num * 2;
 } */
-console.log(generatedFunc()); // RESULT -->  NaN
+console.log(generatedFunc(3)); // RESULT -->  6
 /////////////////////////////////////////////////////
-// Study # 2  //
+console.log("Study # 2 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+function outer() {
+  let counter = 0;
+  function incrementCounter() {
+    counter++;
+  }
+  incrementCounter() // Invoking function in the same scope where it was declared
+}
+outer();
+console.log(outer()); // RESULT --> Undefined
+
+/////////////////////////////////////////////////////
+console.log("Study # 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+
+
+/////////////////////////////////////////////////////
+console.log("Study # 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+
+
 
 /////////////////////////////////////////////////////
 
 
-/////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////
 
 
 /////////////////////////////////////////////////////
---
+++ JS-JavaScript-04192021/testing.js
@@ -0,0 +1,14 @@
+let car = {
+  name: "Toyota",
+  model: "Corolla",
+  year: 1997,
+  hasWifi: false
+}
+console.log(car.name);
+for (let i = 0; i < car.length; i++) {
+  const element = car[i];
+  console.log(element);
+  console.log("For Loop");
+
+}
+
