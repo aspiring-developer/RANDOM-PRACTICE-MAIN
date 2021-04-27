f60b14f3d0735956adfe23839813e426b4c059b8
ssh1sharma <emailtotheowner@gmail.com>
2021-04-27 09:02:54 -0400

    Closure observation

--- JS-JavaScript-04192021/closure_study.js
+++ JS-JavaScript-04192021/closure_study.js
@@ -26,19 +26,63 @@ outer();
 console.log(outer()); // RESULT --> Undefined
 
 /////////////////////////////////////////////////////
-console.log("Study # 3 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~ Study # 3 Lexical Scoping ~~~~~");
 
+function init() {
+  let name = 'Mozilla'; // name is a local variable created by init
+  function displayName() { // displayName() is the inner function, a closure
+    console.log(name); // use variable declared in the parent function
+  }
+  displayName();
+}
+init();
+/////////////////////////////////////////////////////
+console.log("~~~~~ Study # 4 Actual Closure ~~~~~");
 
-
+function makeFunc() {
+  let name = 'Firefox';
+  function displayName() {
+    console.log(name);
+  }
+  console.log(displayName);
+  return displayName;
+}
+let myFunc = makeFunc();
+//console.log(myFunc);
+//console.log(makeFunc);
+//console.log(makeFunc());
+myFunc();
 
 /////////////////////////////////////////////////////
-console.log("Study # 4 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+console.log("~~~~~ Study # 5 Another Closure Example ~~~~~");
 
+function makeAdder(x) {
+  return function(y) {
+    return x + y;
+  };
+}
 
+let add5 = makeAdder(5);
+let add10 = makeAdder(10);
 
-
+console.log(add5(2));  // 7
+console.log(add10(2)); // 12
 /////////////////////////////////////////////////////
+console.log("~~~~~ TESTING ONLY ~~~~~");
 
+function makeAdderTest(x) {
+  function inner(y) {
+    console.log(x + y);
+    return x + y;
+  };
+  return inner;
+}
+
+let add5Test = makeAdderTest(5);
+let add10Test = makeAdderTest(10);
+
+console.log(add5Test(2));  // 7
+console.log(add10Test(2)); // 12
 
 
 
