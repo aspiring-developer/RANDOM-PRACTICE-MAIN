c2e5481f7f4cc72ec3128495b595e77f3825fff9
ssh1sharma <emailtotheowner@gmail.com>
2021-05-04 14:32:47 -0400

    React components

--- NEWER-PRACTICES/react-study-05042021/src/App.js
+++ NEWER-PRACTICES/react-study-05042021/src/App.js
@@ -1,22 +1,26 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  const theName = "Shiva";
+  const theName = "Someone";
   let date = new Date();
   const currentYear = date.getFullYear();
+  const age = 42;
   return (
     <>
 
-      <header> <h1 className="heading"> Just a Test Project </h1></header>
+<Header />
       <div>
-      <img src="https://picsum.photos/id/0/200/300" alt="Random view 1"/>;
-      <img src="https://picsum.photos/id/1005/200/300" alt="Random view 2"/>;
-      <img src="https://picsum.photos/id/1025/200/300" alt="Random view 3"/>;
+      <img src="https://picsum.photos/id/0/150/300" alt="Random view 1"/>;
+      <img src="https://picsum.photos/id/1005/150/300" alt="Random view 2"/>;
+      <img src="https://picsum.photos/id/1025/150/300" alt="Random view 3"/>;
 
       </div>
       <p style={{color: "blue", fontSize: "20px"}}>Created by: {theName} </p>
       {/*Both ways below are fine...*/}
-      <p>Copyright: {currentYear} </p>
+      <p>Birth Year: {currentYear - age} </p>
       <p>Copyright: {date.getFullYear()} </p>
+<Footer />
     </>
   );
 }
---
+++ NEWER-PRACTICES/react-study-05042021/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <>
+      <small>This is from footer component...</small>
+    </>
+  )
+}
+
+export default Footer;
