53daefee6d80e957d0795d8dd0c814b6ade0ee7b
ssh1sharma <emailtotheowner@gmail.com>
2021-05-04 07:51:44 -0400

    Prepare for new project study

--- BC-Refresher-04012021/03-JavaScript/02-Homework/homework-completed-test.js
+++ BC-Refresher-04012021/03-JavaScript/02-Homework/homework-completed-test.js
@@ -1,4 +1,3 @@
-// Retrieving DOM elements
 const characterCounterEl = document.querySelector('#characterCounter');
 const lowerCheckboxEl = document.querySelector('#lowerCheckbox');
 const upperCheckboxEl = document.querySelector('#upperCheckbox');
@@ -7,37 +6,63 @@ const specialCheckboxEl = document.querySelector('#specialCheckbox');
 const passwordResultEl = document.querySelector('#passwordResult');
 const passwordButtonEl = document.querySelector('#passwordButton');
 
-// Given character sets
-const lowerCaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const specialCharacter = ['!', '#', '$', '%', '&', '?', '@', '_'];
-const numberCharacter = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// Creating uppercase characters set from given lowercase set
-let upperCaseCharacter = lowerCaseCharacter.map(function (eachChar) {
-  eachChar = eachChar.toUpperCase();
-  return eachChar;
-});
-console.log(upperCaseCharacter);
+const lowerCaseCharacter = "abcdefghijklmnopqrstuvwxyz";
+const copyCharacter = lowerCaseCharacter;
+const upperCaseCharacter = copyCharacter.toUpperCase();
+const specialCharacter = "!#$%&?@_";
+const numberCharacter = "0123456789";
 
 passwordButtonEl.addEventListener('click', generatePassword);
-
-// Main password generator
 function generatePassword() {
-  let selectedCharacters = [];
+  let checkedCharacters = '';
+
+  // Checking and restricting allowed character count
+  if (characterCounterEl.value < 8 || characterCounterEl.value > 128) {
+    characterCounterEl.value = '';
+    characterCounterEl.focus();
+    alert("Please enter between 8 to 128 number only!");
+  }
+
+  // Checking if the character option is checked or not
   if (!lowerCheckboxEl.checked && !upperCheckboxEl.checked && !numberCheckboxEl.checked && !specialCheckboxEl.checked) {
-    alert("You must check at least one character set!");
+    alert("You must check at least one character option!");
   }
-  else {
-    selectedCharacters.push()
-    randomizedCharacters();
-    console.log(randomizedCharacters())
+
+  // Collecting selected character option
+  if (lowerCheckboxEl.checked) { checkedCharacters += (lowerCaseCharacter); }
+  if (upperCheckboxEl.checked) { checkedCharacters += (upperCaseCharacter); }
+  if (numberCheckboxEl.checked) { checkedCharacters += (numberCharacter); }
+  if (specialCheckboxEl.checked) { checkedCharacters += (specialCharacter); }
+
+  //console.log(checkedCharacters);
+  //console.log(checkedCharacters.length);
+
+  // Randomizing selected characters
+  let randomizedCharacters = '';
+  function randomizeFunc(receivingCharacters) {
+    for (let i = 0; i < characterCounterEl.value; i++) {
+      randomizedCharacters += checkedCharacters.charAt(Math.random() * checkedCharacters.length);
+    }
+    console.log(randomizedCharacters);
+    passwordResultEl.value = randomizedCharacters;
+    return randomizedCharacters;
   }
+  randomizeFunc(checkedCharacters);
 }
+//****************************************** */
 
-// Collection of selected characters
-
-// Randomize selected characters
-function randomizedCharacters() {
-  let randomizedCharacters = Math.random(characterCounterEl.value) * 1
-  return randomizedCharacters;
+function generateRandomNumber(numberOfCharacters) {
+  var randomValues = '';
+  var stringValues = 'ABCDEFGHIJKLMNOabcdefghijklmnopqrstuvwxyzPQRSTUVWXYZ';
+  for (var i = 0; i < numberOfCharacters; i++) {
+    randomValues = randomValues + stringValues.charAt(Math.floor(Math.random() * stringValues.length));
+  }
+  return randomValues;
 }
+console.log(generateRandomNumber(8));
+
+
+
+
+
+
