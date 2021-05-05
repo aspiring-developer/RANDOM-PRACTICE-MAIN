1089d7f69d82966aefdeced7c5b71e613ca40cd8
ssh1sharma <emailtotheowner@gmail.com>
2021-05-05 07:43:37 -0400

    React component study

--- NEWER-PRACTICES/react-study-05042021/src/App.js
+++ NEWER-PRACTICES/react-study-05042021/src/App.js
@@ -1,22 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import ImageData from "./components/ImageData";
 
 function App() {
-  const theName = "Someone";
 
   return (
     <>
-
-<Header />
-      <div>
-      <img src="https://picsum.photos/id/0/150/300" alt="Random view 1"/>;
-      <img src="https://picsum.photos/id/1005/150/300" alt="Random view 2"/>;
-      <img src="https://picsum.photos/id/1025/150/300" alt="Random view 3"/>;
-
-      </div>
-      <p style={{color: "blue", fontSize: "20px"}}>Created by: {theName} </p>
-
-<Footer />
+      <Header />
+      <ImageData />
+      <Footer />
     </>
   );
 }
--- NEWER-PRACTICES/react-study-05042021/src/components/Footer.jsx
+++ NEWER-PRACTICES/react-study-05042021/src/components/Footer.jsx
@@ -4,11 +4,12 @@ const Footer = () => {
   let date = new Date();
   const currentYear = date.getFullYear();
   const age = 42;
+  const theName = "Someone";
   return (
     <>
+      <p style={{ color: "blue", fontSize: "20px" }}>Created by: {theName} </p>
       <small>This is from footer component...</small>
-      {/*Both ways below are fine...*/}
-      <p> Copyright: {date.getFullYear()} | Birth Year: {currentYear - age} </p>
+      <p> Copyright: {currentYear} | Birth Year: {currentYear - age} </p>
     </>
   )
 }
