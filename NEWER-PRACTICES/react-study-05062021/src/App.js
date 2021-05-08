53486173f8c310f2ef040a25f909b5546a7e436c
ssh1sharma <emailtotheowner@gmail.com>
2021-05-08 10:52:14 -0400

    New project environment established

--- NEWER-PRACTICES/react-study-05062021/src/App.js
+++ NEWER-PRACTICES/react-study-05062021/src/App.js
@@ -10,13 +10,11 @@ const App = () => {
       <Header />
       <h1>Item Information:</h1>
       {/*with map method*/}
-{data.map(function(eachItem) {
-  console.log(eachItem);
-  return <DataProcessor imageURL={eachItem.imageURL} imageAlt={eachItem.imageAlt} imageTitle={eachItem.imageTitle} imageDescription={eachItem.imageDescription} imagePrice={eachItem.imagePrice} imageID={eachItem.imageID} />
-}
-)}
-
-
+      {data.map(function (eachItem) {
+        console.log(eachItem);
+        return <DataProcessor imageURL={eachItem.imageURL} imageAlt={eachItem.imageAlt} imageTitle={eachItem.imageTitle} imageDescription={eachItem.imageDescription} imagePrice={eachItem.imagePrice} imageID={eachItem.imageID} />
+      }
+      )}
       {/*without map method...*/}
       {/*<DataProcessor imageURL={data[0].imageURL} imageAlt={data[0].imageAlt} imageTitle={data[0].imageTitle} imageDescription={data[0].imageDescription} imagePrice={data[0].imagePrice} imageID={data[0].imageID} />
       <DataProcessor imageURL={data[1].imageURL} imageAlt={data[1].imageAlt} imageTitle={data[1].imageTitle} imageDescription={data[1].imageDescription} imagePrice={data[1].imagePrice} imageID={data[1].imageID} />
