18ecbe1319abc60cd3b669d4535681443c5f23f7
ssh1sharma <emailtotheowner@gmail.com>
2021-05-08 10:08:47 -0400

    React components and mapping

--- NEWER-PRACTICES/react-study-05062021/src/App.js
+++ NEWER-PRACTICES/react-study-05062021/src/App.js
@@ -1,12 +1,10 @@
-
 import React from 'react';
-import ImageCard from './components/ImageCard';
 
 const App = () => {
   return (
     <>
       <h1>Item Information:</h1>
-      <ImageCard />
+
 
     </>
   )
