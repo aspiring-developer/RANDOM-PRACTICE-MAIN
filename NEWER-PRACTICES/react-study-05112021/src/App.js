ebfe3e573a190bbd9b133f3a0ae1a153d53ff118
ssh1sharma <emailtotheowner@gmail.com>
2021-05-11 11:41:05 -0400

    Mapping dynamically

--- NEWER-PRACTICES/react-study-05112021/src/App.js
+++ NEWER-PRACTICES/react-study-05112021/src/App.js
@@ -5,9 +5,18 @@ import data from './dataFile';
 const App = () => {
   return (
     <>
-<DataProcess imgURL={data[0].link} imgTitle={data[0].title} imgDescription={data[0].description} imgPrice={data[0].price} imgID={data[0].id} />
+      {/*WITH MAP*/}
+{data.map(function(eachItem) {
+  return <DataProcess imgURL={eachItem.link} imgTitle={eachItem.title} imgDescription={eachItem.description} imgPrice={eachItem.price} imgID={eachItem.id} />
+})
+}
+
+
+
+      {/*WITHOUT MAP*/}
+      {/*<DataProcess imgURL={data[0].link} imgTitle={data[0].title} imgDescription={data[0].description} imgPrice={data[0].price} imgID={data[0].id} />
 <DataProcess imgURL={data[1].link} imgTitle={data[1].title} imgDescription={data[1].description} imgPrice={data[1].price} imgID={data[1].id} />
-<DataProcess imgURL={data[2].link} imgTitle={data[2].title} imgDescription={data[2].description} imgPrice={data[2].price} imgID={data[2].id} />
+<DataProcess imgURL={data[2].link} imgTitle={data[2].title} imgDescription={data[2].description} imgPrice={data[2].price} imgID={data[2].id} />*/}
     </>
   )
 }
