a9601f3ca00ff46bfa4f274a8cad3995326ee4b6
ssh1sharma <emailtotheowner@gmail.com>
2021-05-11 08:19:00 -0400

    Local database created

---
+++ NEWER-PRACTICES/react-study-05112021/src/dataFile.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    imgURL: "https://picsum.photos/id/1/200/300",
+    title: "Image Title 101",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos, a!",
+    price: "1000",
+    id: "111"
+  },
+  {
+    imgURL: "https://picsum.photos/id/100/200/300",
+    title: "Image Title 102",
+    description: "Amet consectetur adipisicing elit. Eos, a! Lorem ipsum dolor sit.",
+    price: "2000",
+    id: "222"
+  },
+  {
+    imgURL: "https://picsum.photos/id/1000/200/300",
+    title: "Image Title 103",
+    description: "Eos, a! Lorem ipsum dolor sit amet consectetur adipisicing elit. ",
+    price: "3000",
+    id: "333"
+  }
+
+]
+
+export default data;
