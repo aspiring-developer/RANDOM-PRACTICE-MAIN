a1c5e800f59e30a915ea21afe4d02fbc7b8082ca
ssh1sharma <emailtotheowner@gmail.com>
2021-05-13 07:28:59 -0400

    UseState and useEffect Hooks study

--- NEWER-PRACTICES/react-study-05112021/src/components/StateTest3.jsx
+++ NEWER-PRACTICES/react-study-05112021/src/components/StateTest3.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 
 const StateTest3 = () => {
-const [theTime, setTheTime] = useState(new Date().toLocaleString());
+  const [theTime, setTheTime] = useState(new Date().toLocaleString());
 
-//setTimeout(() => {
+  //setTimeout(() => {
   //setTheTime(new Date().toLocaleString());
-//}, 1000);
+  //}, 1000);
 
   return (
     <>
-<h1>{theTime}</h1>
+      <h1>{theTime}</h1>
     </>
   )
 }
