c3d839372569a118f16a823a784e89bba31459a7
ssh1sharma <emailtotheowner@gmail.com>
2021-05-14 15:48:18 -0400

    React pages and routes

--- NEWER-PRACTICES/react-study-05132021/src/App.js
+++ NEWER-PRACTICES/react-study-05132021/src/App.js
@@ -3,16 +3,18 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 const App = () => {
   return (
-    <>
+    <div style={{fontFamily: "Arial"}}>
+      <MainNavigation />
       <Switch>
         <Route path='/' exact> <AllMeetupsPage /> </Route>
         <Route path='/new-meetup'> <NewMeetupPage /> </Route>
         <Route path='/favorites'> <FavoritesPage /> </Route>
       </Switch>
-    </>
+    </div>
   )
 }
 
