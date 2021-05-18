500018e8a8740da51058e134a46b21e46252e46b
ssh1sharma <emailtotheowner@gmail.com>
2021-05-18 11:47:03 -0400

    Continue on react project

--- NEWER-PRACTICES/react-study-05172021/src/components/ExpenseItem.jsx
+++ NEWER-PRACTICES/react-study-05172021/src/components/ExpenseItem.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  let expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+
   return (
-    <>
-      <h2>Expense Item</h2>
-    </>
+    <div className="expense-item">
+      <div> {expenseDate.toLocaleString()} </div>
+      <div className="expense-item__description"><h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
   )
 }
 
