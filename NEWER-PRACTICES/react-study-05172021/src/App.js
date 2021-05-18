539dae679538c239411ad374037b6cbca828b9bd
ssh1sharma <emailtotheowner@gmail.com>
2021-05-18 12:00:24 -0400

    Continue on react project

--- NEWER-PRACTICES/react-study-05172021/src/App.js
+++ NEWER-PRACTICES/react-study-05172021/src/App.js
@@ -1,9 +1,17 @@
 import ExpenseItem from './components/ExpenseItem';
+import expenses from './dataFile';
 function App() {
   return (
     <>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      {/*<ExpenseItem date={expenses[0].date}  title={expenses[0].title} amount={expenses[0].amount}/>
+      <ExpenseItem date={expenses[1].date}  title={expenses[1].title} amount={expenses[1].amount}/>
+      <ExpenseItem date={expenses[2].date}  title={expenses[2].title} amount={expenses[2].amount}/>
+      <ExpenseItem date={expenses[3].date}  title={expenses[3].title} amount={expenses[3].amount}/>*/}
+
+      {expenses.map(function(eachItem) {
+        return <ExpenseItem date={eachItem.date}  title={eachItem.title} amount={eachItem.amount}/>
+      })}
     </>
   );
 }
--- NEWER-PRACTICES/react-study-05172021/src/components/ExpenseItem.jsx
+++ NEWER-PRACTICES/react-study-05172021/src/components/ExpenseItem.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  let expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div> {expenseDate.toLocaleString()} </div>
-      <div className="expense-item__description"><h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+      <div> {props.date.toLocaleString()} </div>
+      <div className="expense-item__description"><h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   )
