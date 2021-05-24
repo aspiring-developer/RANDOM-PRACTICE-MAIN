dc3889f8e6f042e408e8e5d7d6281fc871cb6b6a
ssh1sharma <emailtotheowner@gmail.com>
2021-05-24 10:51:28 -0400

    Continue on the project

---
+++ NEWER-PRACTICES/react-bt-05212021/src/components/Tasks.jsx
@@ -0,0 +1,30 @@
+const tasks = [
+  {
+    id: 1,
+    text: 'Buy Eggs',
+    day: 'May 21st @ 6:45pm',
+    reminder: 'true'
+  },
+  {
+  id: 2,
+    text: 'Send Money',
+    day: 'May 21st @ 6:50pm',
+    reminder: 'true'
+  },
+  {
+  id: 1,
+    text: 'Purchase Lottery',
+    day: 'May 21st @ 7:00pm',
+    reminder: 'false'
+  }
+]
+
+const Tasks = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Tasks
