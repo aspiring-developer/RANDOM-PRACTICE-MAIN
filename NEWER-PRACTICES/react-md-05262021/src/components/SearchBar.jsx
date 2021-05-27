6b02c3f538324486f177feaf393d02b363eded27
ssh1sharma <emailtotheowner@gmail.com>
2021-05-27 09:49:41 -0400

    Working with input fields and user data

--- NEWER-PRACTICES/react-md-05262021/src/components/SearchBar.jsx
+++ NEWER-PRACTICES/react-md-05262021/src/components/SearchBar.jsx
@@ -2,11 +2,14 @@ import './SearchBar.css';
 import {useState} from 'react';
 
 const SearchBar = () => {
-const [searchValue, setSearchValue] = useState('Search...');
+const [searchValue, setSearchValue] = useState('');
 
 const handleInputChange = (e) => {
   console.log(e.target.value);
   setSearchValue(e.target.value);
+}
+const handleClearClick = () => {
+  setSearchValue('');
 }
 
 
@@ -14,8 +17,7 @@ const handleInputChange = (e) => {
     <div>
       <h1> From Search Bar Component </h1>
       <input type="text" value={searchValue} onChange={handleInputChange}/>
-      <br />
-      {searchValue}
+     <button onClick={handleClearClick}>CLEAR</button>
     </div>
   )
 }
