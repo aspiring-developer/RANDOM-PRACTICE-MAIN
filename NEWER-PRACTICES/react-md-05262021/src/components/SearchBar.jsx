a9b980b11e9eb5bf0afb501453cbceb343a6fa7d
ssh1sharma <emailtotheowner@gmail.com>
2021-05-28 07:52:48 -0400

    New learning environment

--- NEWER-PRACTICES/react-md-05262021/src/components/SearchBar.jsx
+++ NEWER-PRACTICES/react-md-05262021/src/components/SearchBar.jsx
@@ -1,23 +1,30 @@
 import './SearchBar.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
 const SearchBar = () => {
-const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
 
-const handleInputChange = (e) => {
-  console.log(e.target.value);
-  setSearchValue(e.target.value);
-}
-const handleClearClick = () => {
-  setSearchValue('');
-}
+  const handleInputChange = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  }
+  const handleClearClick = () => {
+    setSearchValue('');
+  }
+  const clearButtonToggle = () => {
+    if (searchValue.length > 0) {
+      return <button onClick={handleClearClick}>CLEAR</button>
+    }
+  }
 
+  //const shouldDisplayButton = searchValue.length > 0;
 
   return (
     <div>
       <h1> From Search Bar Component </h1>
-      <input type="text" value={searchValue} onChange={handleInputChange}/>
-     <button onClick={handleClearClick}>CLEAR</button>
+      <input type="text" value={searchValue} onChange={handleInputChange} />
+      {/*{searchValue.length > 0 && <button onClick={handleClearClick}>CLEAR</button>}*/}
+      {clearButtonToggle()}
     </div>
   )
 }
