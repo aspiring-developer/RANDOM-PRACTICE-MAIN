740b3943997c118a7dd0a63fd8d212036e218077
ssh1sharma <emailtotheowner@gmail.com>
2021-07-22 09:20:07 -0400

    Click handler

--- react-study-07222021/src/Home.js
+++ react-study-07222021/src/Home.js
@@ -1,7 +1,22 @@
+//import {useState} from 'react';
 const Home = () => {
+  //const [count, setCount] = useState(0);
+  const handleClick = () => {
+    console.log("You Clicked The Button!");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log(name + ", You Clicked Another Button!");
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <h3>Count: 0</h3>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={()=>{
+        handleClickAgain('Shiva')
+      }}>Click Me Again</button>
     </div>
    );
 }
--- react-study-07222021/src/Navbar.js
+++ react-study-07222021/src/Navbar.js
@@ -3,11 +3,11 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>The React Based Blog</h1>
       <div className="links">
-        <a href="/">Home </a>
+        <a href="/"> Home </a>
         <a href="/create"> New Blog</a>
       </div>
     </nav>
-   );
+  );
 }
 
 export default Navbar;
