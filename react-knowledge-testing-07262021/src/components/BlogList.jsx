6f06a0656fc87ebba01e5d9b8476819afcb6e7aa
ssh1sharma <emailtotheowner@gmail.com>
2021-07-29 09:01:27 -0400

    React study continue

--- react-knowledge-testing-07262021/src/components/BlogList.jsx
+++ react-knowledge-testing-07262021/src/components/BlogList.jsx
@@ -1,9 +1,20 @@
-const BlogList = () => {
+const BlogList = (props) => {
   return (
     <>
-<h2>BlogList Component</h2>
+      <h2>Blog List</h2>
+      {props.blogProp.map(function (eachBlog) {
+        return (
+          <div className="m-2 p-2" key={eachBlog.id}>
+            <h4>{eachBlog.title}</h4>
+            <p>{eachBlog.blogBody}</p>
+            <small>{eachBlog.author}</small>
+            <br />
+            <button onClick={()=>{props.deleteFeatureProp(eachBlog.id)}} className="btn btn-warning p-2">Delete {eachBlog.title} <i className="fa fa-trash"></i></button>
+          </div>)
+      })}
+
     </>
-   );
+  );
 }
 
 export default BlogList;
--- react-knowledge-testing-07262021/src/components/Home.jsx
+++ react-knowledge-testing-07262021/src/components/Home.jsx
@@ -1,7 +1,25 @@
+import BlogList from "./BlogList"
+import React, {useState} from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "First Blog", blogBody: "Blog body of blog1...", author: "Mike Davis", id: 1 },
+    { title: "Next Blog", blogBody: "Blog body of blog2...", author: "Dan Thomas", id: 2 },
+    { title: "Third Blog", blogBody: "Blog body of blog3...", author: "Chris Clark", id: 3 }
+  ]);
+
+  function deleteFeatureFunction(targetedId) {
+    console.log("Working!!!");
+
+      setBlogs(blogs.filter((filteredBlog)=> {
+        return filteredBlog.id !== targetedId;
+      }))
+    }
+
+
   return (
     <>
-<h2>Home Component</h2>
+<BlogList blogProp={blogs} deleteFeatureProp={deleteFeatureFunction}/>
     </>
    );
 }
