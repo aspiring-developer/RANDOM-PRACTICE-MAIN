dbcee713982b25790bc81dae4369f722154b847a
ssh1sharma <emailtotheowner@gmail.com>
2021-07-30 06:56:06 -0400

    New react project initialized

--- react-new-study-test-07302021/src/components/Home.jsx
+++ react-new-study-test-07302021/src/components/Home.jsx
@@ -1,11 +1,20 @@
 import BlogList from './BlogList';
+import React, {useState} from 'react';
 
 const Home = () => {
-  const blogs = [
+
+  const [blogs, setBlogs ]= useState([
     { "title": "First Blog", "blogBody": "First blog body... Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aliquid?", "author": "- Dan Thomas", "id": 100 },
     { "title": "Next Blog", "blogBody": "Next blog body... Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aliquid?", "author": "- Chris Clark", "id": 200 },
     { "title": "Third Blog", "blogBody": "Third blog body... Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, aliquid?", "author": "- George Tsai", "id": 300 },
-  ]
+  ]);
+
+  // Delete Feature
+  function deleteFeatureFunction(targetedId) {
+setBlogs(blogs.filter(function(filteredBlogs){
+  return filteredBlogs.id !== targetedId;
+}))
+  }
 
   return (
     <>
@@ -16,7 +25,7 @@ const Home = () => {
             {/*<div className="input-group mx-auto">
             <input type="text" className="form-control text-center" />
           </div>*/}
-            <BlogList blogProps={blogs} />
+            <BlogList blogProps={blogs} deleteFeatureProps={deleteFeatureFunction}/>
           </div>
         </div>
       </section>
