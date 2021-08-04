0a741ebe7884110548b96a9a92db73133991648b
ssh1sharma <emailtotheowner@gmail.com>
2021-08-04 09:38:32 -0400

    New react refresher project

--- react-new-study-08042021-v1/src/components/Home.jsx
+++ react-new-study-08042021-v1/src/components/Home.jsx
@@ -2,6 +2,7 @@ import BlogList from "./BlogList";
 import { useState, useEffect } from "react";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Delete Feature
   function deleteFeatureFunction(targetedId) {
@@ -10,22 +11,34 @@ const Home = () => {
     }))
   };
 
-  // useEffect Feature
+  // Delete useEffect Feature
   useEffect(() => {
-    fetch('http://localhost:5000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data);
-      })
-  },[])
+setTimeout(() => {
+  fetch('http://localhost:5000/blogs')
+  .then(res => {
+    return res.json()
+  })
+  .then(data => {
+    setBlogs(data);
+    setIsLoading(false);
+  })
+  .catch(err => {
+    console.log(err.message);
+  })
+}, 1000);
+  }, []);
+
+  //// Loading useEffect Feature
+  //useEffect(() => {
+  //  setIsLoading(false);
+  //},[])
 
   return (
     <>
       <section>
         <h3 className="text-dark text-center pt-4" id="greetingTexts"> Blog List </h3>
         {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+        {isLoading && <h1 className="font-weight-bold">LOADING...</h1>}
       </section>
     </>
   );
