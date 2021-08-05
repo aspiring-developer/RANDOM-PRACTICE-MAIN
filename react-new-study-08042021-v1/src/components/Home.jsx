af60f1ec5a7d43437fc7744f73bf8235bfe514a9
ssh1sharma <emailtotheowner@gmail.com>
2021-08-05 14:06:20 -0400

    useEffect hook completed

--- react-new-study-08042021-v1/src/components/Home.jsx
+++ react-new-study-08042021-v1/src/components/Home.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   // Delete Feature
   function deleteFeatureFunction(targetedId) {
@@ -13,31 +14,32 @@ const Home = () => {
 
   // Delete useEffect Feature
   useEffect(() => {
-setTimeout(() => {
-  fetch('http://localhost:5000/blogs')
-  .then(res => {
-    return res.json()
-  })
-  .then(data => {
-    setBlogs(data);
-    setIsLoading(false);
-  })
-  .catch(err => {
-    console.log(err.message);
-  })
-}, 1000);
+    setTimeout(() => {
+      fetch('http://localhost:5000/blogs')
+        .then(res => {
+          if (!res.ok) {
+            throw Error("The fetch request is throwing an error. Check the fetch block of codes.");
+          }
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsLoading(false);
+          setIsError(null);
+        })
+        .catch(err => {
+          setIsLoading(false);
+          setIsError(err.message);
+        })
+    }, 1000);
   }, []);
-
-  //// Loading useEffect Feature
-  //useEffect(() => {
-  //  setIsLoading(false);
-  //},[])
 
   return (
     <>
       <section>
         <h3 className="text-dark text-center pt-4" id="greetingTexts"> Blog List </h3>
         {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+        {isError && <h1 className="font-weight-bold">{isError}</h1>}
         {isLoading && <h1 className="font-weight-bold">LOADING...</h1>}
       </section>
     </>
