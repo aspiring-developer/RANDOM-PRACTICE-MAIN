32e496515838ebd15d8458b966021a58510a747b
ssh1sharma <emailtotheowner@gmail.com>
2021-08-13 06:33:11 -0400

    React simple starter

--- react-new-study-08122021/src/components/BlogList.jsx
+++ react-new-study-08122021/src/components/BlogList.jsx
@@ -1,20 +1,18 @@
 const BlogList = (props) => {
   return (
     props.blogProp.map(eachBlog => {
-      return (
-        <div className="row border border-secondary p-2" key={eachBlog.id}>
-          <div className="col-10">
-            <h4 className="text-secondary pb-3 py-2"> {eachBlog.title} </h4>
-            <small className="text-secondary pb-3 py-2"> {eachBlog.author} </small>
-            <p className="text-secondary pb-3 py-2"> {eachBlog.blogBody} </p>
-          </div>
-          <div className="col-2 text-left d-flex align-items-center">
-            <button type="button" onClick={()=> props.deleteProp(eachBlog.id) } className="btn btn-warning " id="frontLgBtn"> Delete <i className='fa fa-trash'> </i> </button>
-          </div>
+      return <div className="row border border-secondary p-2" key={eachBlog.id}>
+        <div className="col-10 text-dark">
+          <h3>{eachBlog.title}</h3>
+          <small className="text-secondary">{eachBlog.author}</small>
+          <p>{eachBlog.blogBody}</p>
         </div>
-      )
+        <div className="col-2 d-flex align-items-center">
+          <button onClick={() => { props.deleteProp(eachBlog.id) }} className="btn btn-warning">Delete <i className="fa fa-trash"></i></button>
+        </div>
+      </div>
     })
-  )
-}
 
+  );
+}
 export default BlogList;
--- react-new-study-08122021/src/components/Home.jsx
+++ react-new-study-08122021/src/components/Home.jsx
@@ -1,36 +1,51 @@
 import BlogList from "./BlogList";
 import { useState, useEffect } from 'react';
 const Home = () => {
-
   const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
-  // Delete Feature Function
-  const deleteFunction = (targetedId) => {
+  // Delete feature
+  function deleteFunction(targetedId) {
     console.log("Deleting...");
-    setBlogs(blogs.filter(function (filteredBlogs) {
+    setBlogs(blogs.filter(filteredBlogs => {
       return filteredBlogs.id !== targetedId;
-    }))
+    })
+    )
   };
 
-  // useEffect hook
-  useEffect(function () {
-    fetch('http://localhost:5000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data);
-      })
-  }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if (!res.ok) {
+            throw Error("OOPS! Something went wrong while fetching data...")
+          }
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          console.log(err.message);
+          setIsError(err.message);
+          setIsLoading(false);
 
+        })
+    }, 1000);
+
+  }, [])
 
   return (
     <>
       <section>
         <div className="row my-4 mainSection1Row">
           <div className="col p-5 bg-light">
-            <h2 className="text-dark p-3 text-center" id="greetingTexts"> Blog List  </h2>
-            {blogs &&  <BlogList blogProp={blogs} deleteProp={deleteFunction} />}
+            <h2 className="text-dark p-3 text-center"> Blog List  </h2>
+            {isError && <h1 className="text-danger">{isError}</h1>}
+            {isLoading && <h1 className="text-danger">Loading...</h1>}
+            {blogs && <BlogList blogProp={blogs} deleteProp={deleteFunction} />}
           </div>
         </div>
       </section>
@@ -39,3 +54,4 @@ const Home = () => {
 }
 
 export default Home;
+
