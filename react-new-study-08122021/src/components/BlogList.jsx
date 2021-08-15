b31620e8dce454d50283f099479c6310f76665ef
ssh1sharma <emailtotheowner@gmail.com>
2021-08-15 06:31:05 -0400

    React refresher for 08152021

--- react-new-study-08122021/src/components/BlogList.jsx
+++ react-new-study-08122021/src/components/BlogList.jsx
@@ -1,10 +1,19 @@
-const BlogList = () => {
+const BlogList = (props) => {
   return (
     <>
-
+      <div className="row border border-secondary p-2 mb-4">
+        <div className="col-10 text-dark">
+          <h3>Title: {props.blogProps[0].title}</h3>
+          <small className="text-secondary">Author: {props.blogProps[0].author}</small>
+          <p>Body: {props.blogProps[0].blogBody}</p>
+        </div>
+        <div className="col-2 d-flex align-items-center">
+         button goes
+        </div>
+      </div>
 
     </>
-   );
+  );
 }
 
 export default BlogList;
--- react-new-study-08122021/src/components/Home.jsx
+++ react-new-study-08122021/src/components/Home.jsx
@@ -1,34 +1,35 @@
-import BlogList from ".BlogList";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const blogs = [
-      {
-        "title": "First Blog Title",
-        "author": "Shaun Net Ninja",
-        "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
-        "id": 100
-      },
-      {
-        "title": "Second Blog Title",
-        "author": "Brad Traversy",
-        "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
-        "id": 200
-      },
-      {
-        "title": "Third Blog Title",
-        "author": "Angela Yau",
-        "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
-        "id": 300
-      }
-    ];
+    {
+      "title": "First Blog Title",
+      "author": "Shaun Net Ninja",
+      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
+      "id": 100
+    },
+    {
+      "title": "Second Blog Title",
+      "author": "Brad Traversy",
+      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
+      "id": 200
+    },
+    {
+      "title": "Third Blog Title",
+      "author": "Angela Yau",
+      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, error!",
+      "id": 300
+    }
+  ];
 
 
 
   return (
     <>
-<BlogList />
+    <h1 className="m-2 my-4 text-center">Blog List</h1>
+      <BlogList blogProps={blogs} />
     </>
-   );
+  );
 }
 
 export default Home;
