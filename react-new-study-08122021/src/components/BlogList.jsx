c133e4dc1a9ab9afd40704ab55fd0249ab41d13f
ssh1sharma <emailtotheowner@gmail.com>
2021-08-16 07:41:48 -0400

    Another refresher on React

--- react-new-study-08122021/src/components/BlogList.jsx
+++ react-new-study-08122021/src/components/BlogList.jsx
@@ -1,19 +1,19 @@
 const BlogList = (props) => {
   return (
     <>
-   { props.blogProps.map(eachBlog => {
-return (
-  <div className="row border border-secondary p-2 mb-4" key={eachBlog.id}>
-        <div className="col-10 text-dark">
-          <h3>Title: {eachBlog.title}</h3>
-          <small className="text-secondary">Author: {eachBlog.author}</small>
-          <p>Body: {eachBlog.blogBody}</p>
-        </div>
-        <div className="col-2 d-flex align-items-center">
-          <button className="btn btn-warning">Delete <i className="fa fa-trash"></i> </button>
-        </div>
-      </div>
-      )
+      {props.blogProps.map(eachBlog => {
+        return (
+          <div className="row border border-secondary p-2 mb-4" key={eachBlog.id}>
+            <div className="col-10 text-dark">
+              <h3>Title: {eachBlog.title}</h3>
+              <small className="text-secondary">Author: {eachBlog.author}</small>
+              <p>Body: {eachBlog.blogBody}</p>
+            </div>
+            <div className="col-2 d-flex align-items-center">
+              <button onClick={()=>{props.deleteFeatureProp(eachBlog.id)}} className="btn btn-warning">Delete <i className="fa fa-trash"></i> </button>
+            </div>
+          </div>
+        )
       })}
 
 
