45f9ae3e58cdd092eaf6f3c3a33aba2edee71d67
ssh1sharma <emailtotheowner@gmail.com>
2021-08-27 05:43:23 +0545

    Done up to useEffect hook

--- react-study-08272021/src/components/Home.jsx
+++ react-study-08272021/src/components/Home.jsx
@@ -1,40 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from "./BlogList";
 
 const Home = () => {
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [blogs, setBlogs] = useState([
-    {
-      "title": "First Blog Title",
-      "author": "Mike Pence",
-      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 100
-    },
-    {
-      "title": "Second Blog Title",
-      "author": "George Tsai",
-      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 200
-    },
-    {
-      "title": "Third Blog Title",
-      "author": "Chris Clark",
-      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 300
-    }
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   // Delete Feature
   function deleteFeatureFunction(targetedId) {
     console.log("Deleting...");
     setBlogs(blogs.filter(filteredBlogs => {
-return targetedId !== filteredBlogs.id;
+      return targetedId !== filteredBlogs.id;
     }))
-  }
+  };
+
+  // useEffect
+  useEffect(() => {
+ setTimeout(() => {
+  fetch('http://localhost:8000/blogs')
+    .then(res=>{
+      return res.json()
+    })
+      .then(data=>{
+        setBlogs(data);
+        setIsLoading(false)
+      })
+ }, 2000);
+  }, [])
 
   return (
     <>
-      <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />
+      {/*{setIsLoading(isLoading)}*/}
+     {isLoading && <h1 color="red">Loading...</h1>}
+      {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
     </>
   );
 }
