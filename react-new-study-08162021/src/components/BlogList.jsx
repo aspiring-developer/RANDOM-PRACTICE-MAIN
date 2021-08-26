71dc18a23b193fb2f5cd8837aabf13df99d41968
ssh1sharma <emailtotheowner@gmail.com>
2021-08-27 03:14:30 +0545

    Previous commits

--- react-new-study-08162021/src/components/BlogList.jsx
+++ react-new-study-08162021/src/components/BlogList.jsx
@@ -1,20 +1,21 @@
-const BlogList = () => {
+const BlogList = (props) => {
   return (
     <>
-<div className="row p-2 m-1 mb-4 d-flex align-items-center border border-info rounded">
-<div className="col-10">
-<h4>Blog Title: </h4>
-<small className="text-secondary">Author: </small>
-<p>Blog Body: </p>
-</div>
-<div className="col-2">
-  <div className="btn btn-warning">Delete</div>
-</div>
-</div>
-
+      {props.blogProps.map(eachBlog => {
+        return <div className="row p-2 m-1 mb-4 d-flex align-items-center border border-info rounded" key={eachBlog.id}>
+          <div className="col-10">
+            <h4>Blog Title: {eachBlog.title}</h4>
+            <small className="text-secondary">Author: {eachBlog.author}</small>
+            <p>Blog Body: {eachBlog.blogBody}</p>
+          </div>
+          <div className="col-2">
+            <button onClick={() => { props.deleteFeatureProp(eachBlog.id) }} className="btn btn-warning">Delete</button>
+          </div>
+        </div>
+      })}
     </>
 
-   );
+  );
 }
 
 export default BlogList;
--- react-new-study-08162021/src/components/Home.jsx
+++ react-new-study-08162021/src/components/Home.jsx
@@ -1,16 +1,57 @@
-import BlogList from "./BlogList"
+import BlogList from "./BlogList";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  // Delete Feature
+  function deleteFeatureFunction(targetedId) {
+    console.log("Deleting...");
+    setBlogs(blogs.filter((filteredBlogs) => {
+      return filteredBlogs.id !== targetedId;
+    })
+    )
+  };
+
+  // useEffect
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+
+        .then(res => {
+          if (!res.ok) {
+            throw Error("Oops! Something did not work while fetching data.")
+          }
+          return res.json();
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          console.log(err.message);
+          setError(err.message);
+          setIsLoading(false);
+        })
+    }, 1000);
+
+  }, [])
+
   return (
     <>
-    <div className="col mt-4 mb-2">
-<h2>Blog List: </h2>
-</div>
-<div className="col">
-<BlogList />
-</div>
+      <div className="col mt-4 mb-2">
+        <h2>Blog List: </h2>
+      </div>
+      <div className="col">
+        {isLoading && <h1 className="text-warning">Loading...</h1>}
+        {error && <h1 className="text-danger">{error}</h1>}
+        {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+      </div>
+
     </>
-   );
+  );
 }
 
 export default Home;
