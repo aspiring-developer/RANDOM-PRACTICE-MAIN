15f780e114f5ae05510e6f9601c3ac860b0ac255
ssh1sharma <emailtotheowner@gmail.com>
2021-08-30 08:51:55 +0545

    Custom hooks

--- react-study-08272021/src/components/Home.jsx
+++ react-study-08272021/src/components/Home.jsx
@@ -1,47 +1,54 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [blogs, setBlogs] = useState(null);
-  const [error, setError] = useState(null);
+  //const [isLoading, setIsLoading] = useState(true);
+  //const [blogs, setBlogs] = useState(null);
+  //const [error, setError] = useState(null);
+
+  const [dataDelete, setDataDelete] = useState(null);
+
+
+const {isLoading, data, error} = useFetch('http://localhost:8000/blogs');
 
   // Delete Feature
   function deleteFeatureFunction(targetedId) {
     console.log("Deleting...");
-    setBlogs(blogs.filter(filteredBlogs => {
+    setDataDelete(data.filter(filteredBlogs => {
       return targetedId !== filteredBlogs.id;
     }))
   };
 
-  // useEffect
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
-          if (!res.ok) {
-            throw Error('Oops! Something went wrong while fetching data');
-          }
-          return res.json()
-        })
-        .then(data => {
-          setBlogs(data);
-          setIsLoading(false);
-          setError(null);
-        })
-        .catch(err => {
-          console.log(err.message);
-          setError(err.message);
-          setIsLoading(false);
-        })
-    }, 2000);
-  }, [])
+  //// useEffect
+  //useEffect(() => {
+  //  setTimeout(() => {
+  //    fetch('http://localhost:8000/blogs')
+  //      .then(res => {
+  //        if (!res.ok) {
+  //          throw Error('Oops! Something went wrong while fetching data');
+  //        }
+  //        return res.json()
+  //      })
+  //      .then(data => {
+  //        setBlogs(data);
+  //        setIsLoading(false);
+  //        setError(null);
+  //      })
+  //      .catch(err => {
+  //        console.log(err.message);
+  //        setError(err.message);
+  //        setIsLoading(false);
+  //      })
+  //  }, 2000);
+  //}, [])
 
   return (
     <>
       {isLoading && <h1 color="red">Loading...</h1>}
       {error && <h1 color="red">{error}</h1>}
-      {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+      {dataDelete && <BlogList blogProps={data} deleteFeatureProp={deleteFeatureFunction} />}
+      {/*{data && <BlogList blogProps={data}  />}*/}
     </>
   );
 }
---
+++ react-study-08272021/src/components/useFetch.jsx
@@ -0,0 +1,34 @@
+import{useState, useEffect} from 'react';
+
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+      // useEffect
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url)
+        .then(res => {
+          if (!res.ok) {
+            throw Error('Oops! Something went wrong while fetching data');
+          }
+          return res.json()
+        })
+        .then(data => {
+          setData(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch(err => {
+          console.log(err.message);
+          setError(err.message);
+          setIsLoading(false);
+        })
+      }, 2000);
+    }, [url]);
+
+    return {isLoading, data, error};
+
+}
+
+export default useFetch;
