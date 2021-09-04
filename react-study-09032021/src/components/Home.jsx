d0118ec6c39ffc253b30d80202fef4679fd04a7f
ssh1sharma <emailtotheowner@gmail.com>
2021-09-04 11:01:31 +0545

    New project WIP

--- react-study-09032021/src/components/Home.jsx
+++ react-study-09032021/src/components/Home.jsx
@@ -1,35 +1,42 @@
 import BlogList from "./BlogList";
-
+import {useState} from 'react';
 const Home = () => {
-  const blogs = [
-      {
-        "title": "First Blog Title",
-        "author": "Mike Pence",
-        "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-        "id": 100
-      },
-      {
-        "title": "Second Blog Title",
-        "author": "George Tsai",
-        "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-        "id": 200
-      },
-      {
-        "title": "Third Blog Title",
-        "author": "Chris Clark",
-        "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-        "id": 300
-      }
-    ];
+  const [blogs, setBlogs] = useState(
+    [
+    {
+      "title": "First Blog Title",
+      "author": "Mike Pence",
+      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 100
+    },
+    {
+      "title": "Second Blog Title",
+      "author": "George Tsai",
+      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 200
+    },
+    {
+      "title": "Third Blog Title",
+      "author": "Chris Clark",
+      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 300
+    }
+  ]
+  );
 
-    // delete feature
-
+  // delete feature
+  const deleteFeatureFunction = (targetedId) => {
+console.log("Deleting...");
+setBlogs(blogs.filter(filteredBlogs=>{
+  return filteredBlogs.id !== targetedId;
+}))
+  }
 
   return (
     <>
-    <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction}/>
+      <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />
     </>
-   );
+  );
 }
 
 export default Home;
