ee97eb62f87dfd79642978eb593a663af08cd4d4
ssh1sharma <emailtotheowner@gmail.com>
2021-09-04 08:52:04 +0545

    New project WIP

--- react-study-09032021/src/components/BlogList.jsx
+++ react-study-09032021/src/components/BlogList.jsx
@@ -1,19 +1,21 @@
-const BlogList = () => {
-  return (
-    <>
-<section>
-      <div className="row my-4 d-flex align-items-center">
-        <div className="col-10 p-5 bg-light text-center">
-          <h4 className="text-dark p-3" id="greetingTexts"> All Blogs </h4>
-          <p className="text-secondary pb-3 py-2"> Blog Title:  </p>
-          <small className="text-secondary pb-3 py-2"> Author:  </small>
-         </div>
-         <div className="col-2"> <button type="button" className="btn btn-warning " id="frontLgBtn"> Delete </button>
-         </div>
 
-      </div>
-    </section>
-    </>
+const BlogList = (props) => {
+  return (
+    props.blogProps.map(eachBlog => {
+      return (
+        <div className="row my-0 d-flex align-items-center border border-secondary">
+          <div className="col-10 p-5 bg-light text-center">
+            <h4 className="text-dark p-3" id="greetingTexts"> All Blogs </h4>
+            <p className="text-secondary pb-3 py-2"> Blog Title: {eachBlog.title} </p>
+            <small className="text-secondary pb-3 py-2"> Author: {eachBlog.author} </small>
+            <p className="text-secondary pb-3 py-2"> Blog Title: {eachBlog.blogBody} </p>
+          </div>
+          <div className="col-2"> <button type="button" className="btn btn-warning " id="frontLgBtn"> Delete </button>
+          </div>
+        </div>
+     )
+    })
+
   );
 }
 export default BlogList;
--- react-study-09032021/src/components/Home.jsx
+++ react-study-09032021/src/components/Home.jsx
@@ -1,9 +1,30 @@
 import BlogList from "./BlogList";
 
 const Home = () => {
+  const blogs = [
+      {
+        "title": "First Blog Title",
+        "author": "Mike Pence",
+        "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+        "id": 100
+      },
+      {
+        "title": "Second Blog Title",
+        "author": "George Tsai",
+        "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+        "id": 200
+      },
+      {
+        "title": "Third Blog Title",
+        "author": "Chris Clark",
+        "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+        "id": 300
+      }
+    ]
+
   return (
     <>
-    <BlogList />
+    <BlogList blogProps={blogs} />
     </>
    );
 }
