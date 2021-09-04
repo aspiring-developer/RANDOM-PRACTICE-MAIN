8d0e2dad6bc31e2c7350cd6dfe4c57ff16cb12c7
ssh1sharma <emailtotheowner@gmail.com>
2021-09-04 11:58:10 +0545

    New project WIP

--- react-study-09032021/src/components/Home.jsx
+++ react-study-09032021/src/components/Home.jsx
@@ -1,40 +1,41 @@
 import BlogList from "./BlogList";
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 const Home = () => {
-  const [blogs, setBlogs] = useState(
-    [
-    {
-      "title": "First Blog Title",
-      "author": "Mike Pence",
-      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 100
-    },
-    {
-      "title": "Second Blog Title",
-      "author": "George Tsai",
-      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 200
-    },
-    {
-      "title": "Third Blog Title",
-      "author": "Chris Clark",
-      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 300
-    }
-  ]
-  );
+  const [blogs, setBlogs] = useState(null);
+  const [error, setError] = useState(null);
+  //const [loading, setloading] = useState(null);
 
   // delete feature
   const deleteFeatureFunction = (targetedId) => {
-console.log("Deleting...");
-setBlogs(blogs.filter(filteredBlogs=>{
-  return filteredBlogs.id !== targetedId;
-}))
+    console.log("Deleting...");
+    setBlogs(blogs.filter(filteredBlogs => {
+      return filteredBlogs.id !== targetedId;
+    }))
   }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogss')
+        .then(res => {
+          if (!res.ok) {
+            throw Error('Oops! Something went wrong!!')
+          }
+          return res.json()
+            .then(data => {
+              setBlogs(data)
+            })
+            .catch(err => {
+              console.log(err.message);
+              setError(err.message);
+            })
+        })
+    }, 2000);
+  }, [])
 
   return (
     <>
-      <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />
+      {blogs && <BlogList blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+      {error && <h1>{error}</h1>}
     </>
   );
 }
