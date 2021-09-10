48d3e33e91e2b50c41cfb133d1c464b2259163a5
ssh1sharma <emailtotheowner@gmail.com>
2021-09-10 20:59:24 +0545

    Project WIP

--- react-study-09062021/src/components/Home.jsx
+++ react-study-09062021/src/components/Home.jsx
@@ -1,32 +1,33 @@
 import BlogList from "./BlogList";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  const blogs = [
-    {
-      "title": "First Blog Title",
-      "author": "Mike Pence",
-      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 100
-    },
-    {
-      "title": "Second Blog Title",
-      "author": "George Tsai",
-      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 200
-    },
-    {
-      "title": "Third Blog Title",
-      "author": "Chris Clark",
-      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
-      "id": 300
-    }
-  ]
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(null);
+
+  // Delete Feature
+  const deleteFeatureFunction = (targetedId) => {
+    console.log("Deleting...");
+    setBlogs(blogs.filter(filteredBlogs => {
+      return targetedId !== filteredBlogs.id;
+    }))
+  }
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        setBlogs(data)
+      })
+  }, [])
+
 
   return (
     <>
-      <BlogList blogsProp={blogs} />
+      {blogs && <BlogList blogsProp={blogs} deleteFeatureProp={deleteFeatureFunction} />}
     </>
   );
 }
-
 export default Home;
