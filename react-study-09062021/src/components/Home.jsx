d87ef99d9f0165d117db4cd2d0d29cdfbdc8fb01
ssh1sharma <emailtotheowner@gmail.com>
2021-09-10 21:52:28 +0545

    Project WIP

--- react-study-09062021/src/components/Home.jsx
+++ react-study-09062021/src/components/Home.jsx
@@ -3,7 +3,8 @@ import { useState, useEffect } from "react";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-  const [loading, setLoading] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(null);
 
   // Delete Feature
   const deleteFeatureFunction = (targetedId) => {
@@ -14,19 +15,32 @@ const Home = () => {
   }
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json()
-      })
-      .then(data => {
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Oops! Something went wrong while fetching data.');
+          }
+          return res.json()
+        })
+        .then(data => {
+          setBlogs(data)
+          setLoading(false);
+        })
+        .catch(err=>{
+          console.log(err.message);
+          setIsError(err.message);
+          setLoading(false);
+        })
+    }, 2000);
   }, [])
-
 
   return (
     <>
       {blogs && <BlogList blogsProp={blogs} deleteFeatureProp={deleteFeatureFunction} />}
+      {loading && <h1>Loading...</h1>}
+      {Error && <h1>{isError}</h1>}
+      {/*{isError && <h1>{err.message}</h1>}*/}
     </>
   );
 }
