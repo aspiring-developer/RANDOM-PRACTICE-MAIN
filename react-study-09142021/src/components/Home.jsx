c8216bae924f73242549cf09a823a793de8eebb9
ssh1sharma <emailtotheowner@gmail.com>
2021-09-17 06:57:29 +0545

    New Project for today's practice

--- react-study-09142021/src/components/Home.jsx
+++ react-study-09142021/src/components/Home.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import BlogPost from "./BlogPost";
+import { useState } from "react";
 
 export default function Home() {
+  const [blogs, setBlogs] = useState([
+    {
+      "title": "First Blog Title",
+      "author": "Mike Pence",
+      "blogBody": "First Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 100
+    },
+    {
+      "title": "Second Blog Title",
+      "author": "George Tsai",
+      "blogBody": "Second Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 200
+    },
+    {
+      "title": "Third Blog Title",
+      "author": "Chris Clark",
+      "blogBody": "Third Blog Body: Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, nesciunt?",
+      "id": 300
+    }
+  ]);
+
+  // Delete Feature
+const deleteFeatureFunction = (targetedId)=> {
+  console.log("Deleting...");
+  setBlogs(blogs.filter(filteredBlogs=>{
+return targetedId !== filteredBlogs.id;
+  }))
+}
+
+
+
   return (
     <div>
-      <h1>From Home component</h1>
+      <h1> From Home Component </h1>
+      <BlogPost blogProps={blogs} deleteFeatureProp={deleteFeatureFunction} />
     </div>
   )
 }
