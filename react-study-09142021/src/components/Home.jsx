e6533182a81e21efba5fc81d2531bbb8f181b51a
ssh1sharma <emailtotheowner@gmail.com>
2021-09-17 06:19:22 +0545

    New Project for today's practice

--- react-study-09142021/src/components/Home.jsx
+++ react-study-09142021/src/components/Home.jsx
@@ -1,47 +1,9 @@
-import BlogList from "./BlogList";
-import { useState, useEffect } from "react";
+import React from 'react'
 
-const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [isError, setIsError] = useState(null);
-
-  // Delete Feature
-  const deleteFeatureFunction = (targetedId) => {
-    console.log("Deleting...");
-    setBlogs(blogs.filter(filteredBlogs => {
-      return targetedId !== filteredBlogs.id;
-    }))
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch('http://localhost:8000/blogs')
-        .then(res => {
-          if(!res.ok) {
-            throw Error('Oops! Something went wrong while fetching data.');
-          }
-          return res.json()
-        })
-        .then(data => {
-          setBlogs(data)
-          setLoading(false);
-        })
-        .catch(err=>{
-          console.log(err.message);
-          setIsError(err.message);
-          setLoading(false);
-        })
-    }, 2000);
-  }, [])
-
+export default function Home() {
   return (
-    <>
-      {blogs && <BlogList blogsProp={blogs} deleteFeatureProp={deleteFeatureFunction} />}
-      {loading && <h1>Loading...</h1>}
-      {Error && <h1>{isError}</h1>}
-      {/*{isError && <h1>{err.message}</h1>}*/}
-    </>
-  );
+    <div>
+      <h1>From Home component</h1>
+    </div>
+  )
 }
-export default Home;
